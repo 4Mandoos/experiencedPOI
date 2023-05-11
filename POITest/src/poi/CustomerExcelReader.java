7633f04dcb9082380cdb22eb9bb14fe340440bd3
leeheyjin <amideveloper@naver.com>
2023-05-11 20:29:44 +0900

    지겨워

--- POITest/src/poi/CustomerExcelReader.java
+++ POITest/src/poi/CustomerExcelReader.java
@@ -11,41 +11,39 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class CustomerExcelReader {
-	public void reader() {
+public void reader() {
+	try {
+		// FileInputStream을 사용하여 파일을 읽어온다. filePath와 fileNm 변수를 사용하여 파일의 경로와 이름을 지정.
+		FileInputStream file = new FileInputStream(MainApplication.FILE_AFTER_2007);
 
-		try {
-			// FileInputStream을 사용하여 파일을 읽어온다. filePath와 fileNm 변수를 사용하여 파일의 경로와 이름을 지정.
-			FileInputStream file = new FileInputStream(MainApplication.FILE_AFTER_2007);
+		// XSSFWorkbook을 사용하여 엑셀 파일의 내용을 읽어온다. file로부터 XSSFWorkbook 인스턴스를 생성.
+		XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-			// XSSFWorkbook을 사용하여 엑셀 파일의 내용을 읽어온다. file로부터 XSSFWorkbook 인스턴스를 생성.
-			XSSFWorkbook workbook = new XSSFWorkbook(file);
+		// workbook.getSheetAt(0)을 사용하여 첫 번째 시트를 가져온다. XSSFSheet 객체 => 시트를 나타냄
+		XSSFSheet sheet = workbook.getSheetAt(0);
 
-			// workbook.getSheetAt(0)을 사용하여 첫 번째 시트를 가져온다. XSSFSheet 객체 => 시트를 나타냄
-			XSSFSheet sheet = workbook.getSheetAt(0);
+		// 모든 행(row)들을 조회한다.
+		// sheet.iterator() => 시트의 행(row)들을 순회하는 Iterator를 가져온다.
+		Iterator<Row> rowIterator = sheet.iterator();
+		// rowIterator.hasNext() => 다음 행이 있는지 확인
+		while (rowIterator.hasNext()) {
+			// rowIterator.next() => 다음 행을 가져온다.
+			Row row = rowIterator.next();
 
-			// 모든 행(row)들을 조회한다.
-			// sheet.iterator() => 시트의 행(row)들을 순회하는 Iterator를 가져온다.
-			Iterator<Row> rowIterator = sheet.iterator();
-			// rowIterator.hasNext() => 다음 행이 있는지 확인
-			while (rowIterator.hasNext()) {
-				// rowIterator.next() => 다음 행을 가져온다.
-				Row row = rowIterator.next();
+			// 각각의 행에 존재하는 모든 열(cell)을 순회한다. => row와 동일한 방식
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = cellIterator.next();
 
-				// 각각의 행에 존재하는 모든 열(cell)을 순회한다. => row와 동일한 방식
-				Iterator<Cell> cellIterator = row.cellIterator();
-				while (cellIterator.hasNext()) {
-					Cell cell = cellIterator.next();
-
-					// cell.getCellType() => 셀의 타입을 확인
-					// 문자 셀인 경우 cell.getStringCellValue()를 사용하여 값을 가져온다.
-					System.out.print(cell.getStringCellValue() + "\t");
-				}
-				System.out.println();
+				// 문자열 셀 값 가져오기
+				System.out.print(cell.getStringCellValue() + "\t");
 			}
-			file.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println();
 		}
-
+		file.close();
+	} catch (IOException e) {
+		e.printStackTrace();
 	}
+
+}
 }
